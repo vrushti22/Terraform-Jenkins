6406251e512f990e2e5b67be534c5d9c0e8ee361
vrushti22 <94238651+vrushti22@users.noreply.github.com>
2026-02-28 13:27:20 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "my_app_bucket" {
   bucket = "my-terraform-bucket-2026"
 
   tags = {
-    Name        = "My Python App Bucket"
+    Name        = "Bucket of Vrushti"
     Environment = "Dev"
   }
 }
