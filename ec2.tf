a94ebb6a8a793992bdf66fd4ab3c395c7394eb31
vrushti22 <94238651+vrushti22@users.noreply.github.com>
2026-03-06 15:48:35 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,52 @@ resource "aws_instance" "my_instance" {
       Name = "EC2_using_terraform"
     }
 }
+
+//ALB SG
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.project_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+//EC2 SG
+
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.project_vpc.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
