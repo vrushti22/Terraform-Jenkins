67e78f15fed614d3d50480476c474e697c8f5904
vrushti22 <94238651+vrushti22@users.noreply.github.com>
2026-03-06 15:50:30 +0530

    Create ALB.tf

---
+++ ALB.tf
@@ -0,0 +1,41 @@
+resource "aws_lb" "app_lb" {
+  name               = "project-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet.id]
+}
+
+//ALB target group
+
+resource "aws_lb_target_group" "tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.project_vpc.id
+
+  health_check {
+    path = "/web"
+  }
+}
+
+//load balanccer listener
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+//EC2 attachment in target group
+
+resource "aws_lb_target_group_attachment" "tg_attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.private_ec2.id
+  port             = 80
+}
