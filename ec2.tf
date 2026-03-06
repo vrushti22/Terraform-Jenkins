63d192c85d7544d945535b43fc86eca1db51ee45
vrushti22 <94238651+vrushti22@users.noreply.github.com>
2026-03-06 15:52:12 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,12 @@
-resource "aws_instance" "my_instance" {
+resource "aws_instance" "private_ec2" {
     ami = "ami-0ecb62995f68bb549"
     instance_type = "t3.micro"
+    subnet_id     = aws_subnet.private_subnet.id
+    vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+    associate_public_ip_address = false
 
     tags = {
-      Name = "EC2_using_terraform"
+      Name = "private-app-server"
     }
 }
 
