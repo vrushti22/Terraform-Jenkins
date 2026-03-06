c3b5b8b54ebccdbcca7aa1d678f09ed0225c8f7a
vrushti22 <94238651+vrushti22@users.noreply.github.com>
2026-03-06 16:33:57 +0530

    Update and rename s3.tf to backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-project"
+    bucket = "terraform-state-project-vpc"
     key    = "terraform/terraform.tfstate"
     region = "us-east-1"
   }
