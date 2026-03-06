6c94cd9297a8f4d7c2e63cd66c407c74d5ffdfeb
vrushti22 <94238651+vrushti22@users.noreply.github.com>
2026-03-06 15:57:47 +0530

    Create vpc-endpoint.tf

---
+++ vpc-endpoint.tf
@@ -0,0 +1,69 @@
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id            = aws_vpc.project_vpc.id
+  service_name      = "com.amazonaws.us-east-1.ecr.api"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [aws_subnet.private_subnet.id]
+
+  security_group_ids = [aws_security_group.ec2_sg.id]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id            = aws_vpc.project_vpc.id
+  service_name      = "com.amazonaws.us-east-1.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [aws_subnet.private_subnet.id]
+
+  security_group_ids = [aws_security_group.ec2_sg.id]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.us-east-1.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [
+    aws_route_table.private.id
+  ]
+
+  tags = {
+    Name = "s3-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm_messages" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.us-east-1.ssmmessages"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private.id
+  ]
+
+  security_group_ids = [
+    aws_security_group.endpoint_sg.id
+  ]
+
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ec2_messages" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.us-east-1.ec2messages"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids = [
+    aws_subnet.private.id
+  ]
+
+  security_group_ids = [
+    aws_security_group.endpoint_sg.id
+  ]
+
+  private_dns_enabled = true
+}
