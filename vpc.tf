f073a9cab5132c24f1877fbf03785c7b7ff43f64
vrushti22 <94238651+vrushti22@users.noreply.github.com>
2026-03-07 15:52:43 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.project_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "private-subnet"
